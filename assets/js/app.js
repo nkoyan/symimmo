063ed8235990e48534cfe60ec86946184c212c2f
Nkoyan <yoann.kergall@gmail.com>
2019-05-17 19:07:28 +0000

    add assets

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+import '../css/app.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+const $ = require('jquery')
+global.$ = global.jQuery = $;
+
+import 'popper.js'
+import 'bootstrap'
---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PropertyController extends AbstractController
+{
+    /**
+     * @Route("/acheter", name="property_index")
+     */
+    public function index()
+    {
+        return $this->render('property/index.html.twig');
+    }
+}
